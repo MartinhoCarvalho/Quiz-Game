c3035c6c444c898537ff50e3fbd8e659df8dc54e
Martinho Carvalho <martinhocarvalho@MBP-de-Martinho.lan>
2019-10-20 01:13:45 +0100

    Add new containerView

--- Quiz/Quiz/ViewController.h
+++ Quiz/Quiz/ViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import "Quiz.h"
+#import <Foundation/Foundation.h>
 
 @interface ViewController : UIViewController
 @property (assign) Quiz *quiz;
--- Quiz/Quiz/ViewController.m
+++ Quiz/Quiz/ViewController.m
@@ -19,6 +19,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     _quiz = [[Quiz alloc] iniQuestionQuiz:1000 :1 :@"Quem foi o melhor jogador do mundo em 1994?"];
+    
+    UIView *containerView = [[[NSBundle mainBundle] loadNibNamed:@"View" owner:self options:nil] lastObject];
+    [self.view addSubview:containerView];
 }
 
 
